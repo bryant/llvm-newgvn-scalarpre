eaed9c0e3873b33000d51dc18b293050dc26b308
bryant <bryant@users.noreply.github.com>
2017-07-03 11:18:50 -0400

    solve a simpler problem: fully avail

--- include/llvm/Transforms/Scalar/NewLoadPRE.h
+++ include/llvm/Transforms/Scalar/NewLoadPRE.h
@@ -39,12 +39,13 @@ struct PhiOcc final : public Occurrence {
     BasicBlock *Pred;
   };
 
+  PHINode *P = nullptr;
   std::vector<Operand> Defs;
   std::vector<BasicBlock *> Unavail;
-  std::vector<RealOcc *> Uses;
   std::vector<PhiUse> PhiUses;
   bool DownSafe = true;
   bool CanBeAvail = true;
+  bool FullyAvail = true;
 
   // LocalNum is set to 0 to ensure that when sorted in DPO, phi occurrences
   // precede all other occurrences in the same block.
@@ -52,6 +53,9 @@ struct PhiOcc final : public Occurrence {
       : Occurrence{&N, 0, OccPhi} {
     addOperand(FirstOperand, OpBlock);
   }
+
+  PhiOcc(PHINode &P, const DominatorTree &DT)
+      : Occurrence{*DT.getNode(P.getParent()), 0, OccPhi}, P(&P) {}
 
   void addOperand(Occurrence &Occ, BasicBlock &Block) {
     Defs.push_back({&Occ, &Block});
@@ -63,10 +67,6 @@ struct PhiOcc final : public Occurrence {
   void addUse(PhiOcc &P, unsigned OpIdx) { PhiUses.push_back({&P, OpIdx}); }
 
   void addUse(RealOcc &R) { Uses.emplace_back(&R); }
-
-  void resetDownSafe() { DownSafe = false; }
-
-  void resetCanBeAvail() { CanBeAvail = false; }
 
   void verify(const DominatorTree &DT) const;
 
@@ -81,11 +81,8 @@ struct PhiOcc final : public Occurrence {
 
 struct RealOcc final : public Occurrence {
   PointerIntPair<Occurrence *, 1, bool> Def;
-  // ^ Points to the dominating occurrence that makes this RealOcc fully
-  // redundant. nullptr means that this isn't redundant to anything. The bool is
-  // true iff the instruction that this RealOcc wraps is leader of its cong
-  // class.
   PointerUnion<Instruction *, StoreInst *> I;
+  unsigned UseCount = 0;
 
   RealOcc(DomTreeNode &N, unsigned LocalNum, Instruction &I_, bool NewVers)
       : Occurrence{&N, LocalNum, OccReal}, Def(nullptr, NewVers) {
@@ -142,8 +139,10 @@ struct ExitOcc final : public Occurrence {
 // constant time per insertion and deletion.
 struct PiggyBank {
   using Node = Occurrence *;
+  // Indexes into Banks.
   using NodeRef = std::pair<unsigned, unsigned>;
 
+  // Defs, sorted by dom tree depth, for which phis will be placed.
   std::vector<std::vector<Node>> Banks;
   unsigned CurrentLevel;
 
@@ -205,8 +204,8 @@ struct PlaceAndFill {
     DefQ = PiggyBank(Height);
   }
 
-  void addDef(RealOcc &Occ) {
-    // If there are multiple definitions in a block, keepy only the latest
+  void addDef(Occurrence &Occ) {
+    // If there are multiple definitions in a block, keep only the latest
     // because that is the one exposed to phis on the block's DF.
     auto InsPair = Defs.insert({&Occ.getBlock(), {0, 0}});
     if (InsPair.second)
@@ -219,11 +218,9 @@ struct PlaceAndFill {
 
   using PhiMap = DenseMap<const BasicBlock *, PhiOcc>;
 
-  PhiMap calculate() {
-    PhiMap Phis;
+  void calculate(PhiMap &Phis) {
     while (!DefQ.empty())
       visitSubtree(DefQ.pop(), DefQ.CurrentLevel, Phis);
-    return Phis;
   }
 
   void clear() {
@@ -276,7 +273,7 @@ private:
   }
 };
 
-// Enaures that PlaceAndFill is cleared between successive scalarPRE calls.
+// RAII wrapper that ensures its PlaceAndFill is cleared.
 struct ClearGuard {
   PlaceAndFill &Inner;
 
@@ -285,7 +282,9 @@ struct ClearGuard {
   auto addDef(RealOcc &Occ) -> decltype(Inner.addDef(Occ)) {
     return Inner.addDef(Occ);
   }
-  auto calculate() -> decltype(Inner.calculate()) { return Inner.calculate(); }
+  auto calculate(PlaceAndFill::PhiMap &Phis) -> decltype(Inner.calculate()) {
+    return Inner.calculate(Phis);
+  }
 };
 
 template <typename Entry, size_t N> struct DPOStack {
@@ -319,238 +318,93 @@ void PhiOcc::verify(const DominatorTree &DT) const {
   }
 }
 
-// Depth-first propagation of false DownSafe from phi occs to their phi defs.
-void propagateDownUnsafe(std::vector<PhiOcc *> &PhiStack,
-                         DenseMap<const BasicBlock *, PhiOcc> &Phis) {
-  auto pushPhiDefsOf = [&](PhiOcc &P) {
-    for (PhiOcc::Operand &Op : P.Defs)
-      if (auto *PhiDef = dyn_cast<PhiOcc>(Op.Occ))
-        if (PhiDef->DownSafe)
-          PhiStack.push_back(PhiDef);
+// A PhiOcc is fully avail iff each of its operands are dominated by a cong
+// class member (a RealOcc) or a fully avail PhiOcc. The inverse of this is that
+// at least one operand is un-dominated by either of those (a _|_ operand in
+// SSAPRE parlance), or dominated by a phi that is known to be non-fully avail.
+void computeFullyAvail(DenseMap<const BasicBlock *, PhiOcc> &Phis) {
+  SmallVector<PhiOcc *, 32> Stack;
+  auto pushUses = [&](PhiOcc &Phi) {
+    Phi.FullyAvail = false;
+    for (PhiOcc::PhiUse &Use : Phi.PhiUses)
+      if (Use.User->FullyAvail)
+        Stack.push_back(Use.User);
   };
 
-  for (auto &Pair : Phis) {
+  for (auto Pair : Phis) {
+    const BasicBlock *BB = Pair.first;
     PhiOcc &Phi = Pair.second;
-    if (!Phi.DownSafe) {
-      pushPhiDefsOf(Phi);
-      while (!PhiStack.empty()) {
-        PhiOcc *P = PhiStack.back();
-        PhiStack.pop_back();
-        P->resetDownSafe();
-        pushPhiDefsOf(*P);
-      }
+    // Will at least one of Phi's operands be _|_?
+    if (Phi.FullyAvail &&
+        std::distance(pred_begin(BB), pred_end(BB)) < Phi.Defs.size()) {
+      // Yes, so this phi and its direct users will themselves be non-fully
+      // avail. Propagate false by DFS.
+      pushUses(Phi);
+      while (!Stack.empty())
+        pushUses(*Stack.pop_back_val());
     }
   }
 }
 
-// Depth-first propagation of false CanBeAvail from phi occs to their phi users.
-void propagateCantBeAvail(std::vector<PhiOcc *> &PhiStack,
-                          DenseMap<const BasicBlock *, PhiOcc> &Phis) {
-  auto resetAndPushUses = [&](PhiOcc &P) {
-    P.resetCanBeAvail();
-    for (PhiOcc::PhiUse &Use : P.PhiUses)
-      if (Use.User->CanBeAvail && !Use.User->DownSafe)
-        PhiStack.push_back(Use.User);
-  };
-
-  // If PRE insertion is required by one of a phi's operands, then the
-  // corresponding predecessor edge has to be split if critical. And if the edge
-  // is critical but can't be split, then insertion isn't possible and renders
-  // the phi !CanBeAvail.
-  auto critEdgeIssues = [](const PhiOcc &P) {
-    return any_of(P.Unavail, [&](const BasicBlock *B) {
-      // TODO: do this.
-      // return cantPREInsert(*B) || (needsSplit(*B) && cantSplit(*B));
-      return false;
-    });
-  };
-
-  for (auto &Pair : Phis) {
-    PhiOcc &Phi = Pair.second;
-    if (Phi.CanBeAvail &&
-        (!Phi.DownSafe && !Phi.Unavail.empty() || critEdgeIssues(Phi))) {
-      resetAndPushUses(Phi);
-
-      while (!PhiStack.empty()) {
-        PhiOcc *P = PhiStack.back();
-        PhiStack.pop_back();
-        resetAndPushUses(*P);
-      }
-    }
-  }
-}
-
-bool NewGVN::preClass(Function &F, CongruenceClass &Cong, ClearGuard IDFCalc,
-                      std::vector<Occurrence *> ExitOccs) {
-  // const DominatorTree &DT = *DT;
-
-  if (Cong.size() <= 1)
-    // On singleton classes, PRE's sole possible effect is loop-invariant
-    // hoisting. But this is already covered by other loop-hoisting passes.
+// This will find and insert phis at all fully available sites of Cong, which
+// are join points where each predecessor is dominated by a Cong member.
+bool NewGVN::insertFullyAvailPhis(CongruenceClass &Cong, ClearGuard IDFCalc) {
+  if (Cong.size() <= 1 || !isa<BasicExpression>(Cong.getDefiningExpr()))
+    // No possible fully redundant sites in singleton classes.
     return false;
 
+  // Place possible phi markers at DF+ of all Cong members. As they are placed,
+  // each marker will be filled phi operands.
   std::vector<RealOcc> RealOccs;
   RealOccs.reserve(Cong.size());
-  // ^ Important: RealOccs must not and should not reallocate.
-  std::vector<Occurrence *> &DPOSorted = ExitOccs;
-  DPOSorted.reserve(Cong.size() + ExitOccs.size());
+  DenseMap<const BasicBlock *, PhiOcc> Phis;
 
-  // Add a real occurrence for each cong member, an exit occurrence for every
-  // exit block, and phi occurrences at IDFs of each real occurrence (these
-  // possible sites of partial redundancy). Then sort the occurrences into the
-  // order in which each would be encountered during a pre-order walk of the dom
-  // tree.
-
-  // TODO: Not all cong members should be pushed, such as maybe ones with side
-  // effects that won't be eliminated regardless of redundancy.
   for (Value *V : Cong) {
-    Instruction *I = cast<Instruction>(V);
-    RealOccs.emplace_back(*DT->getNode(I->getParent()), InstrToDFSNum(I), *I,
-                          I == Cong.getLeader());
-    DPOSorted.push_back(&RealOccs.back());
-    IDFCalc.addDef(RealOccs.back());
+    if (auto *PN = dyn_cast<PHINode>(V)) {
+      // PN is an existing phi node in the congruence class. Wrap it in a PhiOcc
+      // and toss it in with the other phis. PlaceAndFill will fill this PhiOcc
+      // with Occurrence operands that correspond to the PHINode's.
+      auto InsPair = Phis.insert({PN->getParent(), PhiOcc(*PN, *DT)});
+      IDFCalc.addDef(InsPair->second);
+    } else {
+      Instruction *I = cast<Instruction>(V);
+      RealOccs.emplace_back(*DT->getNode(I->getParent()), InstrToDFSNum(I), *I,
+                            I == Cong.getLeader());
+      IDFCalc.addDef(RealOccs.back());
+    }
   }
 
-  // Phi occurrences are given operands as they are placed. TODO: Since we'll
-  // need to iterate over all phis at several points of this pass, considering
-  // copying the values of Phis to a separate vector.
-  DenseMap<const BasicBlock *, PhiOcc> Phis = IDFCalc.calculate();
-  DPOSorted.reserve(DPOSorted.size() + Phis.size());
-  for (auto &P : Phis) {
+  for (auto &P : Phis)
+    // Verify that PlaceAndFill did its job correctly.
     P.second.verify(*DT);
-    DPOSorted.push_back(&P.second);
+  computeFullyAvail(Phis);
+
+  // Materialize fully available PhiOccs into PHINodes and add into the
+  // congruence class.
+  for (auto &P : Phis) {
+    PhiOcc &Phi = P.second;
+    if (Phi.FullyAvail)
+      if (!Phi.P)
+        Cong.insert(Phi.P = IRBuilder<>(getBlock(), getBlock()->begin())
+                                .CreatePHI(T, Phi.Defs.size()));
   }
 
-  std::sort(DPOSorted.begin(), DPOSorted.end(), [](const Occurrence *A,
-                                                   const Occurrence *B) {
-    unsigned ain = A->in(), bin = B->in(), aout = A->out(), bout = B->out();
-    return std::tie(ain, bout, A->LocalNum) < std::tie(bin, aout, B->LocalNum);
-  });
-
-  // Link uses to defs and eliminate full redundancies wherever possible. This
-  // is just sparsified SSA renaming.
-  DPOStack<Occurrence, 32> Stack;
-  SmallVector<RealOcc *, 32> ProbablyDead;
-  for (Occurrence *Occ : DPOSorted) {
-    assert(Occ);
-    Stack.popUntilDFSScope(*Occ);
-
-    // TODO: Re-order this from most to least likely.
-    //
-    // Possibilities:
-    //
-    // tos:           real    phi
-    // cur:
-    // real           fre     set def, add real use
-    // real, leader   [1]     set down-unsafe
-    // exit           [2]     set down-unsafe
-    // phi            fre     fre
-    //
-    // [1]: Not possible for a congruence leader to be dominated by another
-    // class member.
-    // [2]: Exit occurrences have no effect when exposed to real occurrences.
-
-    // Check the occurrence type of the top of stack.
-    if (!Stack.top()) {
-      // Nothing dominates Occ, so the only thing left is to push it onto the
-      // stack. Exit occs are never pushed because exit blocks dominate nothing.
-      if (!isa<ExitOcc>(Occ))
-        Stack.push(*Occ);
-
-    } else if (auto *PDom = dyn_cast<PhiOcc>(Stack.top())) {
-      if (auto *R = dyn_cast<RealOcc>(Occ)) {
-        if (R->isNewVersion())
-          PDom->resetDownSafe();
-        else
-          R->setDef(*PDom);
-        Stack.push(*R);
-      } else if (auto *P = dyn_cast<PhiOcc>(Occ)) {
-        Stack.push(*P);
-      } else if (auto *Ex = dyn_cast<ExitOcc>(Occ)) {
-        // PDom is directly exposed to an exit and therefore down-unsafe.
-        PDom->resetDownSafe();
-      } else
-        llvm_unreachable("Unexpected occurrence type.");
-
-    } else if (auto *RDom = dyn_cast<RealOcc>(Stack.top())) {
-      // Everything dominated by a real occurrence is fully redundant.
-
-      if (auto *R = dyn_cast<RealOcc>(Occ)) {
-        assert(!R->isNewVersion() && "R is marked new version and thus the "
-                                     "congruence class leader with the lowest "
-                                     "DPO number, yet it's somehow dominated "
-                                     "by another member.");
-        // R is fully redundant to RDom. Don't bother pushing onto the renaming
-        // stack because it's probably dead if it has no side effects, but do
-        // set its def because its phi operand uses need to be updated to RDom
-        // (later).
-        R->setDef(*RDom);
-
-        // Mark its deadness. Quickly short-circuit if a store (which trivially
-        // has side effects).
-        if (!R->getInst().is<StoreInst *>())
-          ProbablyDead.push_back(R);
-      } else if (auto *P = dyn_cast<PhiOcc>(Occ)) {
-        // This phi is fully redundant to RDom and should not have been placed.
-        // TODO: Unnecessary phis are placed when P is in the DF of some R
-        // dominated by RDom. We could prevent this by running FRE before PRE.
-        // P->replaceWith(*RDom);
-      } else if (auto *Ex = dyn_cast<ExitOcc>(Occ)) {
-        // Exposure to exit has no effect on real occurrences.
-      } else
-        llvm_unreachable("Unexpected occurrence type.");
-    }
-  }
-
-  // Update phi operands to most-dominating real occs. Also start tracking which
-  // predecessors are unavailable.
-  for (auto &Pair : Phis) {
-    PhiOcc &Phi = Pair.second;
-    // Fill in unavailable predecessors. TODO: This is quadratic in the number
-    // of predecessors per phi.
-    for (BasicBlock *Pred : predecessors(&Phi.getBlock())) {
-      using Op = PhiOcc::Operand;
-      if (find_if(Phi.Defs, [&](const Op &O) { return O.Pred == Pred; }) ==
-          Phi.Defs.end())
-        Phi.Unavail.push_back(Pred);
-    }
-
-    Phi.verify(*DT);
-
-    // TODO: This could be done later.
-    for (PhiOcc::Operand &Op : Phi.Defs)
-      if (auto *R = dyn_cast<RealOcc>(Op.Occ))
-        Op.Occ = R->getDef() ? R->getDef() : Op.Occ;
-  }
-
-  // Run availability and anticipability analysis on phis.
-  std::vector<PhiOcc *> PhiStack;
-  PhiStack.reserve(Phis.size());
-  propagateDownUnsafe(PhiStack);
-  propagateCantBeAvail(PhiStack);
-
-  // At this point, CanBeAvail will be true for a phi iff it's fully available
-  // (empty Unavail) or partially available (non-empty Unavail) but down-safe
-  // insertions can be made.
-  return false;
-}
-
-bool NewGVN::scalarPRE(Function &F) PROFILE_POINT {
-  // Pre-compute set of exit occurrences as it's the same for all cong classes.
-  std::list<ExitOcc> ExitOccs;
-  std::vector<Occurrence *> Ptrs;
-  for (BasicBlock &BB : F)
-    if (isa<ReturnInst>(BB.getTerminator()) ||
-        isa<UnreachableInst>(BB.getTerminator())) {
-      if (DomTreeNode *Node = DT->getNode(&BB)) {
-        ExitOccs.emplace_back(*Node);
-        Ptrs.push_back(&ExitOccs.back());
+  // Set incoming values of newly inserted PHINodes.
+  for (auto &P : Phis) {
+    PhiOcc &Phi = P.second;
+    // Could be a phi already existing in Cong. Those would already have
+    // incoming values. Skip.
+    if (Phi.FullyAvail && Phi.P->getNumIncomingValues == 0) {
+      for (const PhiOcc::Operand &Op : Phi.Defs) {
+        if (RealOcc *R = dyn_cast<RealOcc>(Op.Occ)) {
+          if (StoreInst *SI = R->getInst().dyn_cast<StoreInst *>())
+            Phi.P->addIncoming(SI->getValueOperand());
+          else
+            Phi.P->addIncoming(R->getInst().dyn_cast<Instruction *>());
+        } else
+          Phi.P->addIncoming(cast<PhiOcc>(Op.Occ.P));
       }
     }
-
-  PlaceAndFill IDF(*DT, F.size());
-  return any_of(CongruenceClasses, [&](CongruenceClass *Cong) {
-    return preClass(F, *Cong, ClearGuard(IDF), Ptrs);
-  });
-}
+  }
+  return true;
 }
